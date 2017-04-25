f2e0f0ad04de3cd5ce2fedad4cf7ae854e215296
NishkarshMakhija <nikhil.makhija11@gmail.com>
2017-04-25 21:50:31 +0530

    Add files via upload

--- class/class.dbConnect.php
+++ class/class.dbConnect.php
@@ -1,1 +1,16 @@
-
+<?php
+class dbConnect
+{
+		function connect()
+		{
+			$con = mysqli_connect("localhost","root","");
+			mysqli_select_db($con,"enigma");
+			return $con;
+		}
+		
+		function execute($query)
+		{
+			return mysqli_query($this->connect(),$query);
+		}
+}
+?>
